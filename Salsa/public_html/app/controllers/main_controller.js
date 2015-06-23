a309434e22667fe5fddae599c841c08a101bcdd6
Ville Talonpoika <talonvi@gmail.com>
2015-06-23 15:20:52 +0300

    Added data.json

--- Salsa/public_html/app/controllers/main_controller.js
+++ Salsa/public_html/app/controllers/main_controller.js
@@ -1,4 +1,7 @@
-Salsa.controller('MainController', ['$scope', function ($scope) {
-       $scope.cards = ["Saab", "Volvo", "BMW"];
-       
+Salsa.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+        $scope.cards = $http.get('../data.json').success(function (response) {
+            console.log(response.data);
+            return response.data;
+        });
+
     }]);
