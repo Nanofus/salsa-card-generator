04ddeceba7de692bb679f63889b31f6c8aa686e0
Ville Talonpoika <talonvi@gmail.com>
2015-06-29 20:06:29 +0300

    Added images, refactoring, stats

--- Salsa/public_html/main_controller.js
+++ Salsa/public_html/main_controller.js
@@ -1,9 +1,7 @@
 Salsa.controller('MainController', ['$scope', '$http', function ($scope, $http) {
         $scope.cards = null;
-        
-        $http.get('data.json').success(function(response){
+
+        $http.get('data.json').success(function (response) {
             $scope.cards = response;
         });
-        
-        console.log($scope.cards);
     }]);
