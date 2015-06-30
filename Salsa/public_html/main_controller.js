2337fd2af3c68710a044627a99daae3f0cbf1b3a
Ville Talonpoika <talonvi@gmail.com>
2015-06-30 03:23:12 +0300

    Added artist credits & card IDs

--- Salsa/public_html/main_controller.js
+++ Salsa/public_html/main_controller.js
@@ -1,6 +1,8 @@
 Salsa.controller('MainController', ['$scope', '$http', function ($scope, $http) {
         $scope.cards = null;
 
+        $scope.version = "Salsa 2.0";
+
         $http.get('data.json').success(function (response) {
             $scope.cards = response;
 
